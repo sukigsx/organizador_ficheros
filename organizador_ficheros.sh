45d4dab6fa53376017624945999c135f62203830
sukigsx <sukigsx.mbsistemas@gmail.com>
2023-11-08 19:41:45 +0100

    Update organizador_ficheros.sh

--- organizador_ficheros.sh
+++ organizador_ficheros.sh
@@ -73,7 +73,7 @@ software_necesario(){
 echo ""
 echo -e " Comprobando el software necesario."
 echo ""
-software="which git diff ping figlet xdotool wmctrl" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
+software="which git diff ping figlet" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
 for paquete in $software
 do
 which $paquete 2>/dev/null 1>/dev/null 0>/dev/null #comprueba si esta el programa llamado programa
@@ -106,7 +106,7 @@ done
 }
 
 # EMPIEZA LO GORDO
-wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz >/dev/null 2>&1
+#wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz >/dev/null 2>&1
 clear
 echo ""
 conexion
@@ -123,7 +123,7 @@ fi
 
 sleep 2
 clear
-wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz
+#wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz
 echo -e "${rosa}"; figlet -c sukigsx; echo -e "${borra_colores}"
 echo ""
 echo -e "${verde} Diseñado por sukigsx / Contacto:   scripts@mbbsistemas.es${borra_colores}"
